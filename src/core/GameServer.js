28ce9977812dbac2f27b1e9f33198453d7ebe771
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2018-11-30 13:46:57 -0600

    Update GameServer.js

--- src/core/GameServer.js
+++ src/core/GameServer.js
@@ -186,36 +186,7 @@ this.name = name;
     this.mainLoopBind = this.mainLoop.bind(this);
 
     // @formatter:off
-    this.colors = [
-      {'r': 255, 'g': 0,   'b': 0  }, // Red
-      {'r': 255, 'g': 32,  'b': 0  },
-      {'r': 255, 'g': 64,  'b': 0  },
-      {'r': 255, 'g': 96,  'b': 0  },
-      {'r': 255, 'g': 128, 'b': 0  }, // Orange
-      {'r': 255, 'g': 160, 'b': 0  },
-      {'r': 255, 'g': 192, 'b': 0  },
-      {'r': 255, 'g': 224, 'b': 0  },
-      {'r': 255, 'g': 255, 'b': 0  }, // Yellow
-      {'r': 192, 'g': 255, 'b': 0  },
-      {'r': 128, 'g': 255, 'b': 0  },
-      {'r': 64,  'g': 255, 'b': 0  },
-      {'r': 0,   'g': 255, 'b': 0  }, // Green
-      {'r': 0,   'g': 192, 'b': 64 },
-      {'r': 0,   'g': 128, 'b': 128},
-      {'r': 0,   'g': 64,  'b': 192},
-      {'r': 0,   'g': 0,   'b': 255}, // Blue
-      {'r': 18,  'g': 0,   'b': 192},
-      {'r': 37,  'g': 0,   'b': 128},
-      {'r': 56,  'g': 0,   'b': 64 },
-      {'r': 75,  'g': 0,   'b': 130}, // Indigo
-      {'r': 92,  'g': 0,   'b': 161},
-      {'r': 109, 'g': 0,   'b': 192},
-      {'r': 126, 'g': 0,   'b': 223},
-      {'r': 143, 'g': 0,   'b': 255}, // Purple
-      {'r': 171, 'g': 0,   'b': 192},
-      {'r': 199, 'g': 0,   'b': 128},
-      {'r': 227, 'g': 0,   'b': 64 }
-    ];
+    this.colors = [{'r':245,'b':0,'g':0},{'r':0,'b':0,'g':245},{'r':0,'b':245,'g':0}]; 
     // @formatter:on
   }
 
@@ -740,7 +711,7 @@ beforeq(player) {
   }
 
   getRandomColor() {
-    return utilities.getRandomColor();
+    return utilities.getOldColor();
   }
 
   // todo change this out for a vector library
